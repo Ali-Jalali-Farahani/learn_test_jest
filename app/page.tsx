c4374f537147c5b90a91f25d415d6d2bcd869e50
Ali-Jalali-Farahani <alijalalfarahani80@gmail.com>
2025-09-06 16:20:12 +0330

    learn Test Error

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,28 @@
-import { PostWithComment } from "../Components/UserInteraction/PostWithComments";
+import { ShoppingList1 } from "Components/Error/ShoppingList1.tsx";
+import { ShoppingList2 } from "Components/Error/ShoppingList2.tsx";
 
-function Home() {
+function App() {
 
+    const ingredients = ['Apples', 'Bananas', 'Ham', 'Bread', 'Bread']
+
+    const someFunction = (selectedItem: string) => {
+        console.log(`Selected ${selectedItem}`)
+    }
 
     return (
         <>
-            <PostWithComment
-                content="The sky is blue"
-                user="Alex"
+            <h2>Shopping list 1:</h2>
+            {/* <ShoppingList1
+        groceries={ingredients}
+        selectItem={someFunction}
+      /> */}
+            <h2>Shopping list 2:</h2>
+            <ShoppingList2
+                groceries={ingredients}
+                selectItem={someFunction}
             />
         </>
     )
 }
 
-export default Home
+export default App
